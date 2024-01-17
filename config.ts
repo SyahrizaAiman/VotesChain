baa009e535bac8355ade787e4b9619eec810e56c
SyahrizaAiman <93464412+SyahrizaAiman@users.noreply.github.com>
2024-01-17 16:16:33 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -3,19 +3,19 @@ import { infuraProvider } from 'wagmi/providers/infura'
  
 export const { chains, publicClient } = configureChains(
   [sepolia],
-  [infuraProvider({ apiKey: '18faa7d5819445ff9be86d23f3c1a121' })],
+  [infuraProvider({ apiKey: 'yourAPIkey' })],
 )
 
 export const config = createConfig({
   publicClient,
 })
 
-export const projectId = 'b1196758d3d12d9cfd25b738e2762e21';
+export const projectId = 'yourProjectID';
 
 export const metadata = {
   name: 'VotesChain',
   description: 'VotesChain',
-  url: 'https://VotesChain.com/',
+  url: 'https://VotesChain.netlify/',
   icons: ['./assets/raise-hand.png'],
   redirect: {
     native: 'YOUR_APP_SCHEME://',
